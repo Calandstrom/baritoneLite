bb09f814430585b4dc5398f33e3b65243a277f4f
Calandstrom <155859914+Calandstrom@users.noreply.github.com>
2025-09-11 14:35:54 -0400

    Baritone Lite 1.21.8

--- src/main/java/baritone/command/manager/CommandManager.java
+++ src/main/java/baritone/command/manager/CommandManager.java
@@ -80,6 +80,13 @@ public class CommandManager implements ICommandManager {
 
     @Override
     public boolean execute(Tuple<String, List<ICommandArgument>> expanded) {
+        String label = expanded.getA();
+    
+        // Only allow "sel" command
+        if (!label.equalsIgnoreCase("sel")) {
+            return false; // block everything else
+        }
+    
         ExecutionWrapper execution = this.from(expanded);
         if (execution != null) {
             execution.execute();
