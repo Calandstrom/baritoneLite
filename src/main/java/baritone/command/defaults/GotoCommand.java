41059c360175c9b1b3f869abf8a04af7ce811429
Calandstrom <155859914+Calandstrom@users.noreply.github.com>
2025-09-26 21:41:58 -0400

    Goto Command Change
    Possibly fixed goto so it no longer path finds to blocks and should only go to coordinates

--- src/main/java/baritone/command/defaults/GotoCommand.java
+++ src/main/java/baritone/command/defaults/GotoCommand.java
@@ -20,13 +20,11 @@ package baritone.command.defaults;
 import baritone.api.IBaritone;
 import baritone.api.command.Command;
 import baritone.api.command.argument.IArgConsumer;
-import baritone.api.command.datatypes.ForBlockOptionalMeta;
 import baritone.api.command.datatypes.RelativeCoordinate;
 import baritone.api.command.datatypes.RelativeGoal;
 import baritone.api.command.exception.CommandException;
 import baritone.api.pathing.goals.Goal;
 import baritone.api.utils.BetterBlockPos;
-import baritone.api.utils.BlockOptionalMeta;
 
 import java.util.Arrays;
 import java.util.List;
@@ -40,47 +38,43 @@ public class GotoCommand extends Command {
 
     @Override
     public void execute(String label, IArgConsumer args) throws CommandException {
-        // If we have a numeric first argument, then parse arguments as coordinates.
-        // Note: There is no reason to want to go where you're already at so there
-        // is no need to handle the case of empty arguments.
-        if (args.peekDatatypeOrNull(RelativeCoordinate.INSTANCE) != null) {
-            args.requireMax(3);
-            BetterBlockPos origin = ctx.playerFeet();
-            Goal goal = args.getDatatypePost(RelativeGoal.INSTANCE, origin);
-            logDirect(String.format("Going to: %s", goal.toString()));
-            baritone.getCustomGoalProcess().setGoalAndPath(goal);
-            return;
+        // Require at least X and Z, at most X Y Z
+        args.requireMin(2);
+        args.requireMax(3);
+
+        // Make sure the first arg looks like a coordinate
+        if (args.peekDatatypeOrNull(RelativeCoordinate.INSTANCE) == null) {
+            throw new CommandException("Invalid arguments: goto only supports coordinates (x z or x y z)");
         }
-        args.requireMax(1);
-        BlockOptionalMeta destination = args.getDatatypeFor(ForBlockOptionalMeta.INSTANCE);
-        baritone.getGetToBlockProcess().getToBlock(destination);
+
+        BetterBlockPos origin = ctx.playerFeet();
+        Goal goal = args.getDatatypePost(RelativeGoal.INSTANCE, origin);
+
+        logDirect(String.format("Going to: %s", goal.toString()));
+        baritone.getCustomGoalProcess().setGoalAndPath(goal);
     }
 
     @Override
     public Stream<String> tabComplete(String label, IArgConsumer args) throws CommandException {
-        // since it's either a goal or a block, I don't think we can tab complete properly?
-        // so just tab complete for the block variant
-        args.requireMax(1);
-        return args.tabCompleteDatatype(ForBlockOptionalMeta.INSTANCE);
+        // No block completions, just return empty
+        return Stream.empty();
     }
 
     @Override
     public String getShortDesc() {
-        return "Go to a coordinate or block";
+        return "Go to a coordinate (x z or x y z)";
     }
 
     @Override
     public List<String> getLongDesc() {
         return Arrays.asList(
-                "The goto command tells Baritone to head towards a given goal or block.",
-                "",
-                "Wherever a coordinate is expected, you can use ~ just like in regular Minecraft commands. Or, you can just use regular numbers.",
-                "",
-                "Usage:",
-                "> goto <block> - Go to a block, wherever it is in the world",
-                "> goto <y> - Go to a Y level",
-                "> goto <x> <z> - Go to an X,Z position",
-                "> goto <x> <y> <z> - Go to an X,Y,Z position"
+            "The goto command tells Baritone to head towards a specific coordinate.",
+            "",
+            "Coordinates can be absolute (numbers) or relative (~ like in Minecraft commands).",
+            "",
+            "Usage:",
+            "> goto <x> <z>",
+            "> goto <x> <y> <z>"
         );
     }
 }
